da039ef5fdc09e84f58f409897cdd1a80886a4ae
Will Metzler <metzlerwill@gmail.com>
2024-02-10 17:43:31 -0500

    Added functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i ++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, message) {
+    let messages = []
+    for (let i = 0; i < names.length; i ++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+};
+    return messages;
+};
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown (i) {
+    while (i >= 0) {
+        console.log(i--);
+    }
+};
+
+countDown(10);
+
+
+
+
+
+
+
+ 
